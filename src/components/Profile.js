fd26e57b8ac48d38a56bfac6ecd859012ac84ddb
svetlanasukova <svitlana.sukova@gmail.com>
2019-04-23 17:37:09 +0300

    css modules

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return(
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img src="https://www.pentair.com/en/_jcr_content/hero/hero-par/hero_image_449393854/image.img.jpg/1532547933301.jpg" />
             </div>
@@ -10,9 +11,9 @@ const Profile = () => {
             <div>
                 my posts
                 <div>new post</div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
                 </div>
             </div>
         </div>
