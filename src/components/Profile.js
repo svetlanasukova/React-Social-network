aef8b94fb68b2adfd48b8fd447eb105ab2711766
svetlanasukova <svitlana.sukova@gmail.com>
2019-04-23 13:04:44 +0300

    components

---
+++ src/components/Profile.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://www.pentair.com/en/_jcr_content/hero/hero-par/hero_image_449393854/image.img.jpg/1532547933301.jpg" />
+            </div>
+            <div> ava + desc</div>
+            <div>
+                my posts
+                <div>new post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
