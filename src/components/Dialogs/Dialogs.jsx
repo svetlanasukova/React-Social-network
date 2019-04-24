bf83c27dff643f8d72c4aba769466d298e7ae976
svetlanasukova <svitlana.sukova@gmail.com>
2019-04-24 14:56:48 +0300

    Rote exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (p) => {
     return(
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Sveta
+                    <NavLink activeClassName={s.active} to="/dialogs/1">Sveta</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink activeClassName={s.active} to="/dialogs/2">Sasha</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Victor
+                    <NavLink activeClassName={s.active} to="/dialogs/3">Victor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Dima
+                    <NavLink activeClassName={s.active} to="/dialogs/4">Dima</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Kate
+                    <NavLink activeClassName={s.active} to="/dialogs/5">Kate</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink activeClassName={s.active} to="/dialogs/6">Valery</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
