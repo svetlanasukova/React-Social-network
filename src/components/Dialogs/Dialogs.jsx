a4fc2581c838561a3f7a602e77dd41c81698bced
svetlanasukova <svitlana.sukova@gmail.com>
2019-04-24 14:40:21 +0300

    страница Диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,30 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (p) => {
     return(
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Kate
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your day?</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
     );
 }
 
